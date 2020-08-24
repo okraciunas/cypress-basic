5104665dc067b7037cda08544ea00a5fc65777b1
Leonardo Oliva Kraciunas <leonardo@okraciunas.com>
2020-08-24 12:24:02 -0300

    Adiciona seleções e simulações de interação nos campos input.

--- cypress/integration/tickets.spec.js
+++ cypress/integration/tickets.spec.js
@@ -13,4 +13,22 @@ describe("Tickets", () => {
     cy.get("#requests").type("Drums!");
     cy.get("#signature").type(`${firstName} ${lastName}`);
   });
+
+  it("select two tickets", () => {
+    cy.get("#ticket-quantity").select("2");
+  });
+
+  it("select 'vip' ticket type", () => {
+    cy.get("#vip").check();
+  });
+
+  it("selects 'social media' checkbox", () => {
+    cy.get("#social-media").check();
+  });
+
+  it("selects 'friend' and 'publication', then uncheck 'friend'", () => {
+    cy.get("#friend").check();
+    cy.get("#publication").check();
+    cy.get("#friend").uncheck();
+  });
 });
